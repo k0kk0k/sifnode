fc8dc7acf04403c1a41ccb92106b81fc342f1a87
James Moore <james@sifchain.finance>
2020-12-24 09:54:02 -0800

    work in progress

--- test/integration/setup_sifchain.sh
+++ test/integration/setup_sifchain.sh
@@ -24,11 +24,20 @@ set_persistant_env_var CHAINDIR $NETWORKDIR/validators/$CHAINNET/$MONIKER $envex
 set_persistant_env_var SIFNODED_LOG $datadir/logs/sifnoded.log $envexportfile
 set_persistant_env_var EBRELAYER_LOG $datadir/logs/ebrelayer.log $envexportfile
 
+rm -f $EBRELAYER_LOG
 mkdir -p $datadir/logs
 nohup $TEST_INTEGRATION_DIR/sifchain_start_daemon.sh > $SIFNODED_LOG 2>&1 &
+set_persistant_env_var SIFNODED_PID $! $envexportfile
 nohup sifnodecli rest-server --laddr tcp://0.0.0.0:1317 > $datadir/logs/restserver.log 2>&1 &
-nohup $TEST_INTEGRATION_DIR/sifchain_start_relayer.sh > $EBRELAYER_LOG 2>&1 &
+set_persistant_env_var REST_SERVER_PID $! $envexportfile
+nohup $TEST_INTEGRATION_DIR/sifchain_start_ebrelayer.sh > $EBRELAYER_LOG 2>&1 &
+set_persistant_env_var EBRELAYER_PID $! $envexportfile
 
 # Wait for ebrelayer to subscribe
+
+while [ ! -f $EBRELAYER_LOG ]
+do
+  sleep 1
+done
 
 tail -f $EBRELAYER_LOG | grep -m 1 "Subscribed"
